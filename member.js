7c5bc3f6a27436e1880a853d3a1a62dad376696e
Vignesh Sitaraman <79943099+vs19g@users.noreply.github.com>
2023-09-27 15:47:43 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,83 @@
-function skillsMember()) {
-  console.log('I am a member of the team.')
+function skillsMember() {
+    this.name = 'skills';
+    this.collectionOptions = {
+        noDefaults: false
+    };
+    this.schema = {
+        name: {
+            type: String,
+            required: true,
+            index: true
+        },
+        level: {
+            type: Number,
+            required: true,
+            default: 1
+        },
+        desc: {
+            type: String,
+            required: false
+        }
+    };
+    this.indexes = [
+        {level: 1}
+    ];
+    this.methods = {
+        // addSkill: function (skill) {
+        //     this.skills.push(skill);
+        //     return this.save();
+        // },
+        // removeSkill: function (skill) {
+        //     this.skills.pull(skill);
+        //     return this.save();
+        // },
+        // updateSkill: function (skill) {
+        //     // TODO
+        //     return this.save();
+        // }
+    };
+    this.statics = {};
+    this.virtuals = {};
+    this.hooks = {};
+}) {
+    this.name = 'skills';
+    this.collectionOptions = {
+        noDefaults: false
+    };
+    this.schema = {
+        name: {
+            type: String,
+            required: true,
+            index: true
+        },
+        level: {
+            type: Number,
+            required: true,
+            default: 1
+        },
+        desc: {
+            type: String,
+            required: false
+        }
+    };
+    this.indexes = [
+        {level: 1}
+    ];
+    this.methods = {
+        // addSkill: function (skill) {
+        //     this.skills.push(skill);
+        //     return this.save();
+        // },
+        // removeSkill: function (skill) {
+        //     this.skills.pull(skill);
+        //     return this.save();
+        // },
+        // updateSkill: function (skill) {
+        //     // TODO
+        //     return this.save();
+        // }
+    };
+    this.statics = {};
+    this.virtuals = {};
+    this.hooks = {};
 }
